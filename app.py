ef59b3269e2505da0ea6e6754dbec957cbf1cc25
Muha-mmed <138076750+Muha-mmed@users.noreply.github.com>
2024-09-01 09:43:31 -0700

    Initial commit of Flask API

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import random
+from quotes import quotes
+
+
+app = Flask(__name__) 
+
+#get all quotes
+@app.route('/')
+def index():
+    return quotes
+
+#get single quote by it's id
+@app.route('/quote/<int:id>')
+def get_quote_by_id(id):
+    for quote in quotes:
+        if quote['id'] == id:
+            return quote
+    return {'error': 'quote not found'}
+
+#get random quote
+@app.route('/rand_quote')
+def get_random_quote():
+    quote = random.choice(quotes)
+    return quote    
+
+if __name__ == ("__main__"):
+    app.run(debug=True)
---
+++ quotes.py
@@ -0,0 +1,323 @@
+quotes= [
+    {
+        'id': 1,
+        'quote': 'I know I ain\'t perfect. But damn, at least a nigga try.'
+    },
+    {
+        'id': 2,
+        'quote': 'In order to keep a 1 track mind on your goals you gotta block out opinions just do you.'
+    },
+    {
+        'id': 3,
+        'quote': 'Everything gon fall in place at the right time.'
+    },
+    {
+        'id': 4,
+        'quote': 'I wanted to get people to feel where I\'m coming from and connect with people who are feeling the same way that I am.'
+    },
+    {
+        'id': 5,
+        'quote': 'The worst thing a person can do is make another person feel worthless.'
+    },
+    {
+        'id': 6,
+        'quote': 'It doesn\'t matter 1 v 100 I\'m gone still stand my ground, regardless.'
+    },
+    {
+        'id': 7,
+        'quote': 'A real gangsta humble enough to walk away from certain situations, everything ain’t about proving a point.'
+    },
+    {
+        'id': 8,
+        'quote': 'God, please watch over me, and please forgive me for all my mistakes.'
+    },
+    {
+        'id': 9,
+        'quote': 'I\'m cool with being me, I\'m not tryna be nobody else.'
+    },
+    {
+        'id': 10,
+        'quote': 'I have to be responsible with my character, my image, and my reputation.'
+    },
+    {
+        'id': 11,
+        'quote': 'I didn\'t want my son to see the same things I had seen growing up or have to go through the same things I went through.'
+    },
+    {
+        'id': 12,
+        'quote': 'Depending on what you do today, you gotta live with each decision that you chose to make.'
+    },
+    {
+        'id': 13,
+        'quote': 'Everything happens in real-time, so make decisions that are easy to live with.'
+    },
+    {
+        'id': 14,
+        'quote': 'Don\'t waste spending yo whole life just chasing after one dream. One passion could be a distraction from yo true calling.'
+    },
+    {
+        'id': 15,
+        'quote': 'Sometimes you gotta acknowledge that you are proud of yourself. F*ck validation from other people.'
+    },
+    {
+        'id': 16,
+        'quote': 'At the end of the day, I\'m willing to rock by myself over being with people that don\'t gat my best interest.'
+    },
+    {
+        'id': 17,
+        'quote': 'I\'m so traumatized from all this pain. Don\'t think I will ever be the same.'
+    },
+    {
+        'id': 18,
+        'quote': 'I knew when I left school, my stomach would probably hurt from having nothing to eat, and I would be going back to those same circumstances that were breaking me.'
+    },
+    {
+        'id': 19,
+        'quote': 'There\'s so much stuff that you see every day that you want to talk about, but I’m not the type to open up, so the only way I could open up is through music.'
+    },
+    {
+        'id': 20,
+        'quote': 'Depending on what you do today, you gotta live with each decision that you chose to make'
+    },
+    {
+        'id': 21,
+        'quote': 'Turned that pain into passion and made it happen.'
+    },
+    {
+        'id': 22,
+        'quote': 'I\'m pretty good at delivering my songs, but I just want to perfect the craft by creating melodies.'
+    },
+    {
+        'id': 23,
+        'quote': 'Since I was 9 years old, I knew how to make a song. But when I got older, I used it as a way to express myself.'
+    },
+    {
+        'id': 24,
+        'quote': 'To all my loved ones in heaven; I see you smiling.'
+    },
+    {
+        'id': 25,
+        'quote': 'I give 70% of loyalty, 20% of love, and 10% of trust'
+    },
+    {
+        'id': 26,
+        'quote': 'Another man\'s success should never make you mad.'
+    },
+    {
+        'id': 27,
+        'quote': 'Nobody love you till you dead, that\'s when they might care'
+    },
+    {
+        'id': 28,
+        'quote': 'Most times I\'m by myself I\'m still confused from betrayel'
+    },
+    {
+        'id': 29,
+        'quote': 'Niggas saying they the goat, but I can\'t buy into the hype. I dig deep into my spirit when I spit into the mic.'
+    },
+    {
+        'id': 30,
+        'quote': 'They weren\'t with me through my struggle, all this shit I sacrifice'
+    },
+    {
+        'id': 31,
+        'quote': 'We hold a grudge and we want blood, we can\'t look pass the issue'
+    },
+    {
+        'id': 32,
+        'quote': 'Happiness and depression I\'m stuck inside the middle.'
+    },
+    {
+        'id': 33,
+        'quote': 'Money only change the ones around you.'
+    },
+    {
+        'id': 34,
+        'quote': 'Nonbelievers listened to your dreams and they clowned you.'
+    },
+    {
+        'id': 35,
+        'quote': 'We was taught to fight no matter what, it ain\'t no giving up.'
+    },
+    {
+        'id': 36,
+        'quote': 'Fuck the industry, it\'s all cap, this shit overrated.'
+    },
+    {
+        'id': 37,
+        'quote': 'I just want a bag, put in that work and grow my fanbase.'
+    },
+    {
+        'id': 38,
+        'quote': 'You ain\'t my color, then you don\'t know the struggle of livin\' black.'
+    },
+    {
+        'id': 39,
+        'quote': 'I made it to the top they all looked up and disbelieve'
+    },
+    {
+        'id': 40,
+        'quote': 'They hate i\'m doing better, they\'d much try get rid of me'
+    },
+    {
+        'id': 41,
+        'quote': 'I came a long way but i ain\'t far as i\'m finna be.'
+    },
+    {
+        'id': 42,
+        'quote': 'I promise i\'ma die a legend i\'ma make sure you remember me.'
+    },
+    {
+        'id': 43,
+        'quote': 'Hope my secret get berried when i\'m gone.'
+    },
+    {
+        'id': 44,
+        'quote': 'thinking about too real i lost my mind in the scope.'
+    },
+    {
+        'id': 45,
+        'quote': 'Can\'t relapse off these drug, man, R.I.P. to Juice. '
+    },
+    {
+        'id': 46,
+        'quote': 'i know death come unexpected you can\'t choose a day.'
+    },
+    {
+        'id': 47,
+        'quote': 'thinking about the old me, i swear i missed you dearly.'
+    },
+    {
+        'id': 48,
+        'quote': 'every day a battle, i\'m exhausted and i\m weary.'
+    },
+    {
+        'id': 49,
+        'quote': 'fuck a sack up at Neimans i spent some racks on my appearance.'
+    },
+    {
+        'id': 50,
+        'quote': 'Everybody going through something it\'s all about persevering.'
+    },
+    {
+        'id': 51,
+        'quote': 'i fought through it all the shit hurt me severely.'
+    },
+    {
+        'id': 52,
+        'quote': 'They said i\'m Pac rebirth, never put out a weak verse.'
+    },
+    {
+        'id': 53,
+        'quote': 'They was counting me out i put passion in every lyric'
+    },
+    {
+        'id': 54,
+        'quote': 'i\'m from Chicago, she said she love my accent'
+    },
+    {
+        'id': 55,
+        'quote': 'i\'m from Chicago where Glocks what we react with'
+    },
+    {
+        'id': 56,
+        'quote': 'Loving me ain\'t easy, if you leave i don\'t blame you '
+    },
+    {
+        'id': 57,
+        'quote': 'Watch how i move, wrong decision can be fatal'
+    },
+    {
+        'id': 58,
+        'quote': 'Most time i\'m by myself, i\'m still confused from betrayal'
+    },
+    {
+        'id': 59,
+        'quote': 'Every day my birthday, bitch, it\'s lit, i just turn twenty-one'
+    },
+    {
+        'id': 60,
+        'quote': 'I did a lot of shit in these streets but only got booked for trapping'
+    },
+    {
+        'id': 61,
+        'quote': 'And the day i leave this earth, i\'ma die with legend status'
+    },
+    {
+        'id': 62,
+        'quote': 'Nonbelievers listened to your dreams and they clowned you'
+    },
+    {
+        'id': 63,
+        'quote': 'My Good heart become a weakness i did everything i can.'
+    },
+    {
+        'id': 64,
+        'quote': 'No point in church \'cause the preacher can\'t keep them poles from clapping.'
+    },
+    {
+        'id': 65,
+        'quote': 'God, please watch over me, and please forgive me for all my mistakes.'
+    },
+    {
+        'id': 66,
+        'quote': 'I know I ain\'t perfect. But damn, at least a nigga try.'
+    },
+    {
+        'id': 67,
+        'quote': 'In order to keep a 1 track mind on your goals you gotta block out opinions… just do you.'
+    },
+    {
+        'id': 68,
+        'quote': 'Everything gon fall in place at the right time.'
+    },
+    {
+        'id': 69,
+        'quote': 'I wanted to get people to feel where I\'m coming from and connect with people who are feeling the same way that I am.'
+    },
+    {
+        'id': 70,
+        'quote': 'The worst thing a person can do is make another person feel worthless.'
+    },
+    {
+        'id': 71,
+        'quote': 'I\'m cool with being me, I\'m not tryna be nobody else.'
+    },
+    {
+        'id': 72,
+        'quote': 'It doesn\'t matter 1 v 100 I\'m gone still stand my ground, regardless.'
+    },
+    {
+        'id': 73,
+        'quote': 'Don\'t waste spendin\' yo whole life just chasin\' after one dream. One passion could be a distraction from yo true calling.'
+    },
+    {
+        'id': 74,
+        'quote': 'A real gangsta humble enough to walk away from certain situations, everything ain\'t about proving a point.'
+    },
+    {
+        'id': 75,
+        'quote': 'Sometimes you gotta acknowledge that you are proud of yourself. F*ck validation from other people.'
+    },
+    {
+        'id': 76,
+        'quote': 'I have to be responsible with my character, my image, and my reputation.'
+    },
+    {
+        'id': 77,
+        'quote': 'I didn\'t want my son to see the same things I had seen growing up or have to go through the same things I went through.'
+    },
+    {
+        'id': 78,
+        'quote': 'Depending on what you do today, you gotta live with each decision that you chose to make.'
+    },
+    {
+        'id': 79,
+        'quote': 'Everything happens in real-time, so make decisions that are easy to live with.'
+            
+    },
+    {
+        'id': 80,
+        'quote': 'R.I.P the juice'
+    }
+]
